f99ba763039551359814caa8b594644e75e75238
Josué Silvério <josueaosilverio@gmail.com>
2026-02-25 16:34:13 +0000

    Quartz sync: Feb 25, 2026, 4:34 PM

---
+++ content/index.md
@@ -0,0 +1,5 @@
+---
+title: Pathfinder 2E Session Notes
+---
+
+Pathfinder 2E Cheliax Session Notes and other info
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Pathfinder 2E Cheliax Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "josueaosilverio.github.io/pf2e-cheliax-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
